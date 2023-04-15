df6b3957aef3dc74e8726109013aac9de1e899a0
Ram <guptaram94@gmail.com>
2023-04-15 18:10:39 +0530

    sign up flow

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+require('./db/config');
+const User = require('./db/User');
 const app = express();
 
-app.get("/", (req, resp) =>
+app.use(express.json());
+app.use(cors());
+app.post('/register', async (req, resp) =>
 {
-    resp.send("Apps is working .....");
+    let user = new User(req.body);
+    let result = await user.save();
+    resp.send(result);
+    console.log(result);
 });
+
+
+/* const connectDB = async () =>
+{
+    mongoose.connect('mongodb://localhost:27017/e-comm');
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model('products', productSchema);
+    const data = await product.find();
+    console.warn(data);
+
+}
+connectDB(); */
 
 app.listen(5000);
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import Nav from './Nav';
+import Nav from './components/Nav';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './Footer';
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
+import PrivateComponent from './components/PrivateComponent'
 
 function App()
 {
@@ -10,11 +12,14 @@ function App()
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path='/' element={<h1>Products Listing Component</h1>} />
-          <Route path='/add' element={<h1>Add Products Component</h1>} />
-          <Route path='/update' element={<h1>Update Products Component</h1>} />
-          <Route path='/logout' element={<h1>Logout Component</h1>} />
-          <Route path='/profile' element={<h1>Profile Component</h1>} />
+          <Route element={<PrivateComponent />}>
+            <Route path='/' element={<h1>Products Listing Component</h1>} />
+            <Route path='/add' element={<h1>Add Products Component</h1>} />
+            <Route path='/update' element={<h1>Update Products Component</h1>} />
+            <Route path='/logout' element={<h1>Logout Component</h1>} />
+            <Route path='/profile' element={<h1>Profile Component</h1>} />
+          </Route>
+          <Route path='/signup' element={<SignUp />} />
         </Routes>
         {/* <h1>E-Dashboard</h1> */}
         {/* <Footer /> */} {/* we use routing in Footer then use Footer Component in BrowserRouter */}
